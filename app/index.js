8b28656db35fa9e65f486cb95404cbfb3f756865
EgorStasevich <staiger30052000@gmail.com>
2019-07-16 05:54:39 +0300

    fix css

--- app/index.js
+++ app/index.js
@@ -45,10 +45,6 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 app.use('/', routes);
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+
 
 module.exports = app;
